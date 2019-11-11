d3538b49ecf21999a0b6058c4e315f2d38fab66a
xueqiuyun <xueqiuyun@wondersgroup.com>
2019-11-11 15:11:54 +0800

    测试nodejs+express

---
+++ service.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const app=express();
+//express 实例化
+const  bodypaser=require('body-parser');
+//qpp.use  使用中间件
+//解析表单数据
+app.use(bodypaser.urlencoded({extended:false}))
+//最简单的api接口
+app.get('/user/login',(req,res)=>{
+    console.log('你好');
+    //接收参数
+    // let {us,ps}=req.query;
+    res.send('注册成功');
+})
+
+app.post('/user/reg',(req,res)=>{
+    // let {us,ps}=req.body;
+    console.log(req.body);
+    res.send('注册成功');
+
+})
+
+app.listen(3000,()=>{
+    console.log('server start');
+})
